6a6afe40a8160bbd670302ee36b22d3a3dc209f0
olivereld <programadorctg@gmail.com>
2022-06-20 11:05:22 -0400

    [TASK-1819] - [Fix] : Fixed footer

--- src/app/shared/footer/app.footer.component.ts
+++ src/app/shared/footer/app.footer.component.ts
@@ -6,17 +6,25 @@ import {AppComponent} from '../../app.component';
     selector: 'app-footer',
     template: `
         <div id="Footer" class="section flex align-items-center justify-content-center flex-column layout-menu-light layout-footer lg:flex-row" style="text-align: center;">
-            © 2022 Desarrollado por
-            <a href="http://www.jirka.co/" title="Ir a Jirka" style="color: #398bf7;"target="_blank"><b>Jirka</b> </a>
+             {{'© 2022 Desarrollado por '}}
+            <a href="http://www.jirka.co/" title="Ir a Jirka"target="_blank"><b>Jirka</b> </a>
         </div>
     `,
     styles:[`
     #Footer{
+        display:flex;
+        flex-direction: row;
+        justify-content:center;
         width:100%;
-        height:27px;
+        height:37px;
         background-color:#E91E63;
-        color:white;       
+        color:white;            
     }
+    #Footer a{
+        font-weight: 600;
+        text-decoration: none;
+        color: white !important;
+    }   
     `]
 })
 export class AppFooterComponent {
